90fe5c471241b1037e1b7425d5eaa3065306a8dd
AULA F.DE ING.DE SISTEMAS E INF <AULAFISI@UNMSM.pe>
2019-06-13 19:45:32 -0500

    cambio de credenciales BD

--- app.py
+++ app.py
@@ -96,7 +96,7 @@ def upload():
 
 #CONEXION A LA BD
 def connect_database():
-    return ps.connect(host="67.205.143.180", port=5432, dbname="tcs2", user="modulo4", password="modulo4")
+    return ps.connect(host="67.205.143.180", port=5432, dbname="testcarga01", user="modulo4", password="modulo4")
 
 #
 #path_zip_file = DESTINATION
